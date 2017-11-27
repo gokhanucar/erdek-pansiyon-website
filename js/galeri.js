9e8501bb0913b40c534da7bebf3665b80a10ff59
Gökhan Uçar <gokhanucar10@gmail.com>
2017-11-27 23:30:48 +0300

    Add files via upload

---
+++ js/galeri.js
@@ -0,0 +1,324 @@
+var openPhotoSwipe_zemin_kat = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+        // build items array
+	var items = [
+            {
+                src: 'images/galeri/daireler/giris-kat/01.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/02.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/03.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/04.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/05.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/06.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/07.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/08.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/09.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/giris-kat/10.jpg',
+                w: 1200,
+                h: 675
+            }
+        ];
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+var openPhotoSwipe_teras_kat = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+        // build items array
+	var items = [
+            {
+                src: 'images/galeri/daireler/teras-kat/01.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/02.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/03.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/04.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/05.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/06.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/07.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/08.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/teras-kat/09.jpg',
+                w: 1200,
+                h: 675
+            }
+        ];
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+var openPhotoSwipe_orta_kat = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+        // build items array
+	var items = [
+            {
+                src: 'images/galeri/daireler/orta-on/01.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/02.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/03.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/04.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/05.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/06.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/07.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/orta-on/08.jpg',
+                w: 1200,
+                h: 675
+            }
+        ];
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+    
+var openPhotoSwipe_kirmizi_ev = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+        // build items array
+	var items = [
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/01.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/02.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/03.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/04.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/05.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/06.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/daireler/kirmizi-ev/07.jpg',
+                w: 1200,
+                h: 675
+            }
+        ];
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+    
+var openPhotoSwipe_ocaklar = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+        // build items array
+	var items = [
+            {
+                src: 'images/galeri/ocaklar/01.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/02.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/03.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/04.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/05.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/06.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/07.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/08.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/09.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/10.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/11.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/12.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/13.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/14.jpg',
+                w: 1200,
+                h: 675
+            },
+            {
+                src: 'images/galeri/ocaklar/15.jpg',
+                w: 1200,
+                h: 675
+            }
+        ];
+        // define options (if needed)
+        var options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+        // Initializes and opens PhotoSwipe
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
